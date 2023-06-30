d3cb46dfd790a9a0e702be6b5e3f0b728bb18223
Haritanagarajan <30harita2002@gmail.com>
2023-06-30 13:15:42 +0530

    react

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -28,71 +28,67 @@ const Cart = () => {
     </>);
 
     return (
-        <div>
-            <div className="row justify-content-center">
-                <div className="col-12 text-center mt-5">
-
-                    <div className="col-auto ms-auto">
-                        <h1>Your Bag total is ₹{cartTotal}</h1>
-                    </div>
-                    <br />
-                    <table className="table">
-                        <tbody>
-                            <tr>
-                                <td style={{ padding: "0px 100px 0px 10px" }}> </td>
-                                <td style={{ padding: "0px 100px 0px 10px" }}> </td>
-                                <td style={{ padding: "0px 50px 0px 0px" }}></td>
-                                <td style={{ padding: "0px 50px 0px 0px" }}></td>
-                            </tr>
-
-                            {items.map((item, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>
-                                            <img src={item.image} style={{ height: "25rem" }} alt="" />
-                                        </td>
-                                        <td style={{ fontSize: '20px', fontWeight: '500' }}>{item.title}<h6 className='ms-n5' style={{ fontWeight: '600',fontSize:'13px' }}>Size:{item.Size}</h6></td>
-                                        <button
-                                            style={{ fontWeight: 'bold', fontSize: '20px', border: 'none' }}
-                                            className=" ms-5" type='button'
-                                            onClick={() =>
-                                                updateItemQuantity(item.id, item.quantity - 1)
-                                            }
-                                        >
-                                            –
-                                        </button>
-                                        <b>{item.quantity}</b>
-                                        <button
-                                            style={{ fontWeight: 'bold', fontSize: '20px', border: 'none' }}
-                                            className="" type='button'
-                                            onClick={() =>
-                                                updateItemQuantity(item.id, item.quantity + 1)
-                                            }
-                                        >
-                                            +
-                                        </button>
-                                        <td style={{ fontWeight: '600', fontSize: '20px', border: 'none' }}>₹{totalprice(item)}</td>
-
-                                        <button
-                                            className="btn mt-n5" style={{ color: 'red',fontSize:'14px' }}
-                                            onClick={() => removeItem(item.id)}
-                                        >
-                                          Remove
-                                        </button>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-                <br />
+        <div className="row justify-content-center" >
+            <div className="col-12 text-center mt-5">
 
                 <div className="col-auto ms-auto">
-                    <button className="btn btn-danger m-2" onClick={() => emptyCart()}>
-                        Clear Cart
-                    </button>
-                    <button className="btn btn-primary m-2" onClick={backtoschool}>Continue Shopping</button>
+                    <h1>Your Bag total is ₹{cartTotal}</h1>
                 </div>
+                <br />
+                <table className="table" >
+                    <tbody>
+                        {items.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td style={{ border: 'none' }}>
+                                        <img src={item.image} style={{ height: "25rem" }} alt="" onClick={backtoschool} />
+                                    </td>
+                                    <td style={{ fontSize: '20px', fontWeight: '500', border: 'none' }}>{item.title}<h6 className='ms-n5' style={{ fontWeight: '600', fontSize: '13px' }}>Size:{item.Size}</h6></td>
+                                    <button
+                                        style={{ fontWeight: 'bold', fontSize: '20px', border: 'none' }}
+                                        className=" ms-5" type='button'
+                                        onClick={() =>
+                                            updateItemQuantity(item.id, item.quantity - 1)
+                                        }
+                                    >
+                                        –
+                                    </button>
+                                    <b>{item.quantity}</b>
+                                    <button
+                                        style={{ fontWeight: 'bold', fontSize: '20px', border: 'none' }}
+                                        className="" type='button'
+                                        onClick={() =>
+                                            updateItemQuantity(item.id, item.quantity + 1)
+                                        }
+                                    >
+                                        +
+                                    </button>
+                                    <td style={{ fontWeight: '600', fontSize: '20px', border: 'none' }}>₹{totalprice(item)}</td>
+
+                                    <button
+                                        className="btn mt-3 ms-n3" style={{ color: 'red', fontSize: '11px', fontWeight: '600' }}
+                                        onClick={() => removeItem(item.id)}
+                                    >
+                                        Remove
+                                    </button>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div className="col-12-auto ms-auto">
+                <h1 style={{ color: '#978F8F', fontSize: '20px' }} className='float-end ms-n5'>Sub Total ₹{cartTotal}</h1>
+            </div>
+            <div className="col-12-auto ms-auto" >
+                <h1 style={{ color: 'black', fontSize: '30px' }} className='float-end ms-n5'>Total ₹{cartTotal}</h1>
+            </div>
+
+            <div className="col-auto ms-auto">
+                <button className="btn btn-danger m-2" onClick={() => emptyCart()}>
+                    Clear Cart
+                </button>
+                <button className="btn btn-primary m-2" onClick={backtoschool}>Continue Shopping</button>
             </div>
         </div>
     );
